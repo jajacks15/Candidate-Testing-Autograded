4ffdd9cdc5f2ddc36bfc6f8c17ef661b16922038
Julian Jackson <jjackson2@jjackson2-23.local>
2024-01-28 02:19:59 -0500

    Graded Assignment 1 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,17 +3,23 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = " ";
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = " ";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -24,23 +30,48 @@ candidateName = input.question("Please enter your name: ");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i++) { 
+  let responses = input.question(questions[i]);
+   candidateAnswers.push(responses)
+   };
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("Correct");
-} else {
-  console.log("Incorrect");
+// if (candidateAnswer == correctAnswer) {
+//   console.log("Correct");
+// } else {
+//   console.log("Incorrect");
+// }
+// console.log(`You answered: ${candidateAnswers}. The correct answers: are ${correctAnswers}, respectively.`)
+function caseInsensitive(arr) {
+  return arr.toLocaleString().toLowerCase().split(",");
+}
+correctAnswers = caseInsensitive(correctAnswers);
+candidateAnswers = caseInsensitive(candidateAnswers);
+
+let answeredCorrectly = 0
+
+for (let i =0; i < correctAnswers.length; i++) {
+ if (candidateAnswers[i] == correctAnswers[i]){
+  answeredCorrectly += 1
+ } else {
+  answeredCorrectly += 0
+ }
 }
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade = (answeredCorrectly / questions.length) * 100;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
+
+  if (grade > 80) {
+    console.log(`You scored ${grade}%. Congratulations, you passed!`);
+  } else {
+    console.log(`You scored ${grade}%. You have failed.`)
+  }
 }
 
 function runProgram() {
